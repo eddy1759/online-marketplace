0ac3afd3c1cfa37db21b1ea0402cdfc6b002f8b2
Edet Asuquo <eddy1759@gmail.com>
2023-10-11 01:17:27 +0100

    fix: fixed the roles and permsission

--- config/roles.js
+++ config/roles.js
@@ -1,48 +1,33 @@
 const buyer = {
-	permissions: [
-		'createOrder',
-		'editOrder',
-		'deleteOrder',
-		'addToCart',
-		'removeFromCart',
-		'viewOrders',
-		'sendMessage',
-	],
+	permissions: ['createOrder', 'editOrder', 'deleteOrder', 'addToCart', 'removeFromCart', 'viewOrders', 'sendMessage'],
 	roles: ['buyer'],
-};
-
-const seller = {
-	permissions: [
-		'createProduct',
-		'editProduct',
-		'viewOrders',
-		'sendMessage',
-		'fulfillOrder',
-	],
+  };
+  
+  const seller = {
+	permissions: ['createProduct', 'editProduct', 'viewOrders', 'sendMessage', 'fulfillOrder'],
 	roles: ['seller'],
-};
-
-const admin = {
-	permissions: ['manageUsers', 'approveProducts'],
+  };
+  
+  const admin = {
+	permissions: [...buyer.permissions, ...seller.permissions, 'manageUsers', 'approveProducts'],
 	roles: ['admin'],
-};
-
-const superAdmin = {
-	permissions: ['manageAdmins'],
+  };
+  
+  const superAdmin = {
+	permissions: [...admin.permissions, 'manageAdmins', 'performSuperAdminActions'],
 	roles: ['superAdmin'],
-};
-
-const allRoles = [buyer, seller, admin, superAdmin];
-
-const roles = allRoles.reduce((acc, role) => {
+  };
+  
+  const allRoles = [buyer, seller, admin, superAdmin];
+  
+  const roles = allRoles.reduce((acc, role) => {
 	return acc.concat(role.roles);
-}, []);
-
-const roleRights = new Map(
-	allRoles.map((role) => [role.roles[0], role.permissions]),
-);
-
-module.exports = {
+  }, []);
+  
+  const roleRights = new Map(allRoles.map((role) => [role.roles[0], role.permissions]));
+  
+  module.exports = {
 	roles,
 	roleRights,
-};
+  };
+  
