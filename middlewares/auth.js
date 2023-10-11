ad86da0a70b4c776e3bd3887867970a25b5428e7
Edet Asuquo <eddy1759@gmail.com>
2023-10-11 01:52:58 +0100

    feat: auth middleware added

---
+++ middlewares/auth.js
@@ -0,0 +1,38 @@
+const httpStatus = require('http-status');
+const { roleRights } = require('../config/roles');
+const ApiError = require('../utils/ApiError');
+
+const verifyCallback = (req, resolve, reject, requiredRights) => {
+    return async (err, user, info) => {
+      if (err || info || !user) {
+        reject(new ApiError(httpStatus.UNAUTHORIZED, 'Please authenticate'));
+        return;
+      }
+      req.user = user;
+  
+      if (requiredRights.length) {
+        const userRights = roleRights.get(user.role);
+        const hasRequiredRights = requiredRights.every((requiredRight) => userRights.includes(requiredRight));
+  
+        if (!hasRequiredRights && req.params.userId !== user.id) {
+          reject(new ApiError(httpStatus.FORBIDDEN, 'Forbidden'));
+          return;
+        }
+      }
+  
+      resolve();
+    };
+};
+  
+const auth = (...requiredRights) => {
+    return async (req, res, next) => {
+        try {
+            await verifyCallback(req, requiredRights);
+            next();
+        } catch (error) {
+            next(error);
+        }
+    }
+}
+
+module.exports = auth;
